4fcbee0d5480f3fc1ef9f16ea4df1edb54b60df0
hadjoullis <andreashadjoullis@pm.me>
2025-12-27 00:50:10 +0100

    Create driver for serial port (used for bochs logging)

---
+++ serial_driver.c
@@ -0,0 +1,117 @@
+#include "io.h"
+
+/* The I/O ports */
+
+/* All the I/O ports are calculated relative to the data port. This is because
+ * all serial ports (COM1, COM2, COM3, COM4) have their ports in the same
+ * order, but they start at different values.
+ */
+
+#define SERIAL_COM1_BASE                0x3F8      /* COM1 base port */
+
+#define SERIAL_DATA_PORT(base)          (base)
+#define SERIAL_FIFO_COMMAND_PORT(base)  (base + 2)
+#define SERIAL_LINE_COMMAND_PORT(base)  (base + 3)
+#define SERIAL_MODEM_COMMAND_PORT(base) (base + 4)
+#define SERIAL_LINE_STATUS_PORT(base)   (base + 5)
+
+/* The I/O port commands */
+
+/* SERIAL_LINE_ENABLE_DLAB:
+ * Tells the serial port to expect first the highest 8 bits on the data port,
+ * then the lowest 8 bits will follow
+ */
+#define SERIAL_LINE_ENABLE_DLAB         0x80
+
+/** serial_configure_baud_rate:
+ *  Sets the speed of the data being sent. The default speed of a serial port
+ *  is 115200 bits/s. The argument is a divisor of that number, hence the
+ *  resulting speed becomes (115200 / divisor) bits/s.
+ *
+ *  @param com      The COM port to configure
+ *  @param divisor  The divisor
+ */
+static void serial_configure_baud_rate(unsigned short com,
+                                       unsigned short divisor) {
+    outb(SERIAL_LINE_COMMAND_PORT(com), SERIAL_LINE_ENABLE_DLAB);
+    outb(SERIAL_DATA_PORT(com), (divisor >> 8) & 0x00FF);
+    outb(SERIAL_DATA_PORT(com), divisor & 0x00FF);
+}
+
+/** serial_configure_line:
+ *  Configures the line of the given serial port. The port is set to have a
+ *  data length of 8 bits, no parity bits, one stop bit and break control
+ *  disabled.
+ *
+ *  @param com  The serial port to configure
+ */
+static void serial_configure_line(unsigned short com) {
+    /* Bit:     | 7 | 6 | 5 4 3 | 2 | 1 0 |
+     * Content: | d | b | prty  | s | dl  |
+     * Value:   | 0 | 0 | 0 0 0 | 0 | 1 1 | = 0x03
+     */
+    outb(SERIAL_LINE_COMMAND_PORT(com), 0x03);
+}
+
+/** serial_configure_fifo:
+ *  Configures the fifo of the given serial port. The port is set to enable
+ *  FIFO, clear both receiver and transmission FIFO queues and use 14 bytes as
+ *  size of queue.
+ *
+ *  @param com  The serial port to configure
+ */
+static void serial_configure_fifo(unsigned short com) {
+    /* Bit:     | 7 6 | 5  | 4 | 3   | 2   | 1   | 0 |
+     * Content: | lvl | bs | r | dma | clt | clr | e |
+     * Value:   | 1 1 | 0  | 0 | 0   | 1   | 1   | 1 |
+     */
+    outb(SERIAL_FIFO_COMMAND_PORT(com), 0xC7);
+}
+
+/** serial_configure_modem:
+ *  Configures the modem of the given serial port. The port is set to have both
+ *  RTS and DTR set to 1, meaning data is ready to be sent. Interrupts are not
+ *  enabled since no data is received.
+ *
+ *  @param com  The serial port to configure
+ */
+static void serial_configure_modem(unsigned short com) {
+    /* Bit:     | 7 | 6 | 5  | 4  | 3   | 2   | 1   | 0   |
+     * Content: | r | r | af | lb | ao2 | ao1 | rts | dtr |
+     * Content: | 0 | 0 | 0  | 0  | 0   | 0   | 1   | 1   |
+     */
+    outb(SERIAL_MODEM_COMMAND_PORT(com), 0x03);
+}
+
+/** serial_is_transmit_fifo_empty:
+ *  Checks whether the transmit FIFO queue is empty or not for the given COM
+ *  port.
+ *
+ *  @param  com The COM port
+ *  @return 0 if the transmit FIFO queue is not empty
+ *          1 if the transmit FIFO queue is empty
+ */
+static int serial_is_transmit_fifo_empty(unsigned int com) {
+    /* 0x20 = 0010 0000 */
+    return inb(SERIAL_LINE_STATUS_PORT(com)) & 0x20;
+}
+
+int serial_write(char *buf, unsigned int len) {
+    serial_configure_baud_rate(SERIAL_COM1_BASE, 2);
+    serial_configure_line(SERIAL_COM1_BASE);
+    serial_configure_fifo(SERIAL_COM1_BASE);
+    serial_configure_modem(SERIAL_COM1_BASE);
+
+    if (len == 1) {
+        while (!serial_is_transmit_fifo_empty(SERIAL_COM1_BASE)) { ; }
+        outb(SERIAL_DATA_PORT(SERIAL_COM1_BASE), buf[0]);
+        return 0;
+    }
+
+    for (unsigned int i = 0; i < len - 1; i++) {
+        while (!serial_is_transmit_fifo_empty(SERIAL_COM1_BASE)) { ; }
+        unsigned short chars = buf[i + 1] << 8 | buf[i];
+        outb(SERIAL_DATA_PORT(SERIAL_COM1_BASE), chars);
+    }
+    return 0;
+}
---
+++ serial_driver.h
@@ -0,0 +1,6 @@
+#ifndef INCLUDE_SERIAL_DRIVER_H
+#define INCLUDE_SERIAL_DRIVER_H
+
+int serial_write(char *buf, unsigned int len);
+
+#endif /* INCLUDE_SERIAL_DRIVER_H */
