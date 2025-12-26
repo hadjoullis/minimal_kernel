30b457795ca5f775c1f642c821880658ce0bef56
hadjoullis <andreashadjoullis@pm.me>
2025-12-26 19:57:06 +0100

    Fix bugs with usage of ':'

--- loader.s
+++ loader.s
@@ -10,11 +10,11 @@ align 4                     ; align at 4 bytes
 kernel_stack:               ; label points to beginning of memory
     resb KERNEL_STACK_SIZE  ; reserve stack for the kernel
 
-section .text:              ; start of the text (code) section
-align 4                     ; the code must be 4 byte aligned
-dd MAGIC_NUMBER             ; write the magic number to the machine code,
-dd FLAGS                    ; the flags,
-dd CHECKSUM                 ; and the checksum
+section .text               ; start of the text (code) section
+    align 4                 ; the code must be 4 byte aligned
+    dd MAGIC_NUMBER         ; write the magic number to the machine code,
+    dd FLAGS                ; the flags,
+    dd CHECKSUM             ; and the checksum
 loader:                     ; the loader label (defined as entry point in linker script)
     mov esp, kernel_stack + KERNEL_STACK_SIZE   ; point esp to the start of the
                                                 ; stack (end of memory area)
@@ -26,5 +26,5 @@ loader:                     ; the loader label (defined as entry point in linker
     push dword 1            ; arg1
     call sum_of_three       ; call the function, the result will be in eax
 
-.loop
+.loop:
     jmp .loop
