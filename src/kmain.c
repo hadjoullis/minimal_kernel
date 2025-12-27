5307c3f825cb1aff2616688c18dd1a6d8e4fd009
hadjoullis <andreashadjoullis@pm.me>
2025-12-27 17:05:49 +0100

    Test myio with kmain

--- src/kmain.c
+++ src/kmain.c
@@ -1,9 +1,10 @@
-#include "fb_driver.h"
-#include "serial_driver.h"
+#include "myio.h"
 
 int main(void) {
     char msg[] = "hello world!\n";
-    fb_write(msg, sizeof(msg) - 1);
-    serial_write(msg, sizeof(msg) - 1);
+    myprintf(msg, sizeof(msg) - 1);
+    mylog(MYIO_LOG_INFO, "foo\n", 4);
+    mylog(MYIO_LOG_DEBUG, "bar\n", 4);
+    mylog(MYIO_LOG_ERROR, "baz\n", 4);
     return 0;
 }
